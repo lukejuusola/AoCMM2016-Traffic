5976201f2c4f8a5e0ff439075e4b91295287aed8
sunash <sunash.sharma@gmail.com>
2016-10-15 18:43:03 -0700

    Update readin.py

--- readin.py
+++ readin.py
@@ -7,18 +7,17 @@ def file_len(fname):
     return i + 1
 
 # Filename here
-filename = 'outHr5.txt'
-points = file_len(filename)
-data = numpy.zeros((4, points))
-ind = 0
-f = open(filename)
-for line in f:
-	temp = line.split()
-	print temp
-	tempFl = numpy.array([0.0, 0.0, 0.0, 0.0])
-	for x in range(4):
-		tempFl[x] = float(temp[x])
-	data[:, ind] = tempFl
-	ind += 1
-print data
-		
+def readData(filein):
+	filename = filein
+	points = file_len(filename)
+	data = numpy.zeros((4, points))
+	ind = 0
+	f = open(filename)
+	for line in f:
+		temp = line.split()
+		tempFl = numpy.array([0.0, 0.0, 0.0, 0.0])
+		for x in range(4):
+			tempFl[x] = float(temp[x])
+		data[:, ind] = tempFl
+		ind += 1
+	return data
