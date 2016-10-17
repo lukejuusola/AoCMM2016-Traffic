5f068bc29d6ecd840503ddf15d6610161e2c21ac
nmaskara <nish.mask@gmail.com>
2016-10-17 16:14:45 -0700

    changed to function

--- Plot.py
+++ Plot.py
@@ -4,16 +4,16 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
-if(__name__ == "__main__"):
+def plot(f, leftX, rightX, leftY, rightY):
 	fig = plt.figure()
 	ax = fig.gca(projection='3d')
-	data = [(.5,.5), (-.5, -.5), (1, 1), (4, -1)]
-	cmap = CrashMap(data, 1, 1)
-	vcmap = np.vectorize(cmap)
-	X = np.arange(-5, 5, .1)
-	Y = np.arange(-5, 5, .1)
+	vf = np.vectorize(f)
+	X = np.linspace(leftX, rightX, 100)
+	Y = np.linspace(leftY, rightY, 100)
 	X, Y = np.meshgrid(X, Y)
-	Z = vcmap(X, Y)
+	Z = vf(X, Y)
+        #surf = ax.contourf(X, Y, Z)
+        #plt.contourf(X, Y, Z)
 	surf = ax.plot_surface(X,Y,Z, rstride=2, cstride=2, cmap=cm.coolwarm,
                        linewidth=0, antialiased=False)
 	plt.show()
