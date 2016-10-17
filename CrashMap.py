0ca93637231905b517e5cf692c23cb47b72fa103
nmaskara <nish.mask@gmail.com>
2016-10-17 14:38:54 -0700

    Wrote Gradient function. Removed dataset dependency of CrashMap

--- CrashMap.py
+++ CrashMap.py
@@ -2,19 +2,21 @@ import math
 from scipy.integrate import dblquad, IntegrationWarning
 import numpy as np
 import warnings
+import copy
 warnings.simplefilter("ignore", IntegrationWarning)
 warnings.simplefilter("ignore", UserWarning)
 
-manhattan_x = (-1, 1)
-manhattan_y = (-1, 1)
+manhattan_x = (-10, 10)
+manhattan_y = (-10, 10)
 
 #Assume dataset is in form [(x_0, y_0), ..., (x_n, y_n)] where x, y is gps coordinates
 def CrashMap(dataset, stdx, stdy):
+        new_dataset = copy.deepcopy(dataset)
 	def freqMap(x, y):
 		z = 0.0
 		C = 1.0 # Normalization constant. Definitely needs to be tweeked
 		        # Should just be able to divide in the end.
-		for x_i,y_i in dataset:
+		for x_i,y_i in new_dataset:
 			dx = (x - x_i)
 			dy = (y - y_i)
 			exponent = -(dx**2/(2*stdx**2) + dy**2/(2*stdy**2))
